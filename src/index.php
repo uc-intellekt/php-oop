d19f5b897c0a90e095f372650f8395a7052eeb58
Bocharsky Victor <bocharsky.bw@gmail.com>
2015-06-16 23:49:24 +0300

    Create new object of Post class

--- src/index.php
+++ src/index.php
@@ -1,1 +1,8 @@
 <?php
+
+$post = new Post();
+$post->heading = 'My first post';
+$post->content = 'The content of my first post...';
+
+echo '<h1>' . $post->heading . '</h1>';
+echo '<p>' . $post->content . '</p>';

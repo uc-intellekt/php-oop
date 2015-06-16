6294c336764169f9b4a0a0721bd8fd0f502d73c6
Bocharsky Victor <bocharsky.bw@gmail.com>
2015-06-17 00:24:00 +0300

    Update Post.php

--- src/Blog/Post.php
+++ src/Blog/Post.php
@@ -2,12 +2,32 @@
 
 class Post
 {
-    public $heading = 'Untitled';
+    private $heading = 'Untitled';
     
-    public $content = '';
+    private $content = '';
     
     public function read()
     {
         echo 'Read the "' . $this->heading . '"';
+    }
+    
+    public function getHeading()
+    {
+        return $this->heading;
+    }
+    
+    public function setHeading($heading)
+    {
+        $this->heading = $heading;
+    }
+    
+    public function getContent()
+    {
+        return $this->content;
+    }
+    
+    public function setContent($content)
+    {
+        $this->content = $content;
     }
 }
